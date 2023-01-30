8ef0e9af011407ccb24b160eb68879650078ea7b
Daniel Bittan Graells <danielbittang@gmail.com>
2023-01-30 23:06:35 +0100

    update

--- Almacenamiento/CRUD - Cookies/js/script.js
+++ Almacenamiento/CRUD - Cookies/js/script.js
@@ -1,18 +1,7 @@
 "use strict"
-    var productos=getCookieValue("product");
-    var marcas=getCookieValue("brand");
-    var precio=getCookieValue("price");
-    var estado=getCookieValue("state");
     
 //Envía los datos a la tabla creando una nueva fila
-var row="<tr class='bg-white border-b dark:bg-gray-800 dark:border-gray-700'><th scope='row' class='px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white'>"+productos+"</th><td class='px-6 py-4'>"+marcas+"</td><td class='px-6 py-4'>"+precio+"</td><td class='px-6 py-4'>"+estado+"</td><td class='px-6 py-4'><button id='borrar' class='rounded-lg m-auto hover:bg-red-700 bg-red-500 text-white p-2 w-20'>borrar</button></td></tr>" 
-    document.getElementById("cuerpo").innerHTML+=row;
 
 //Un click al boton sube las cookies.
-cargar.addEventListener("click",subirCookies);
-
-//Botón de borrado
-borrar.addEventListener("click",eliminarCookies);
-
-console.log(productos +" "+marcas+" "+precio+" "+estado);
+cargar.addEventListener("click",guardarDatos);
 
--- Almacenamiento/CRUD - Cookies/js/storage.js
+++ Almacenamiento/CRUD - Cookies/js/storage.js
@@ -1,27 +1,16 @@
 "use strict"
 
+var opciones=radio.value;
+console.log(opciones);
 //Dar valores a una cookie
 
-var pr=["si"];
-var price=["1999"];
-var bra=["nike"];
-var sta=["good"];
-
 // Funciones de las cookies
-document.cookie="product="+pr+';path=/;';
-document.cookie="brand="+bra+';path=/;';
-document.cookie="price="+price+';path=/;';
-document.cookie="state="+sta+';path=/;';
-
-function subirCookies(){
-	pr.push(document.getElementById("prod").value);
-	price.push(document.getElementById("pre").value);
-	bra.push(document.getElementById("brand").value);
-	sta.push(document.getElementById("state").value);
+function guardarDatos(){
+	const objeto={product:}
 }
-console.log(pr);
+
 // ***COOKIES***
-// function subirCookies(){
+// function guardarDatos(){
 // 	document.cookie=cookieName+"="+prod.value;
 // 	document.cookie=cookieBrand+"="+brand.value;
 // 	document.cookie=cookiePre+"="+pre.value;
@@ -31,18 +20,10 @@ console.log(pr);
 // Borrar una cookie especificada
 
 function eliminarCookies() {
-	document.cookie='product=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
-	document.cookie='brand=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
-	document.cookie='price=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
-	document.cookie='state=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
 }
 
 //Función que obtiene los valores de la cookie recibiendo el nombre.
 
-function getCookieValue(name) {
-    var encuentra = document.cookie.match(new RegExp("(^| )" + name + "=([^;]+)"));
-    return encuentra ? encuentra[2] : "";
-}
 
 //LocalStorage
 
