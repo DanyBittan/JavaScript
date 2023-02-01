1bd9e49c0a82877814b766ae6f727834ce321f30
DanyBittan <115544090+dbitgra@users.noreply.github.com>
2023-02-01 19:18:39 +0100

    update

--- Almacenamiento/CRUD - Cookies/js/script.js
+++ Almacenamiento/CRUD - Cookies/js/script.js
@@ -8,8 +8,8 @@ let cookieValues=getCookie("data");
 let objTransform=JSON.parse(cookieValues);
 console.log(objTransform);
 objTransform.foreach(valor){
-	document.createElement("<tr>");
-	document.createElement("")
+	let div=" <tr class='bg-white border-b dark:bg-gray-800 dark:border-gray-700'><th scope='row' class='px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white'>"+valor.product+"</th><td class='px-6 py-4'>"+valor.marca+"</td><td class='px-6 py-4'>"+valor.price+"</td><td class='px-6 py-4'>$2999</td></tr>"
+	cuerpo.appendChild("div");
 }
 
 
