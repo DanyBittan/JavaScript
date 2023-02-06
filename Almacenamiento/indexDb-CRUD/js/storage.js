05c8ac473c49fb10086456dfc4cb3eb5df953b21
DanyBittan <115544090+dbitgra@users.noreply.github.com>
2023-02-06 20:12:34 +0100

    update

--- Almacenamiento/indexDb-CRUD/js/storage.js
+++ Almacenamiento/indexDb-CRUD/js/storage.js
@@ -1,11 +1,35 @@
 "use strict"
 
 let openDB=indexedDB.open("productos",1);
+let db;
 
-openDB.onerror=(e)=>{
+openDB.onerror=function(e){
 	console.log('Error de indexDB: ${requestDB.errorCode}');
 }
 
-openDB.onsuccess=(e)=>{
-	let db=e.target.result;
+openDB.onupgradeneeded=function(){
+	let db=openDB.result;
+	if(!db.objectStoreNames.contains('producto')){
+		db.createObjectStore("producto",{keypath: "id", autoIncrement: true});
+	}
+	console.log("creada");
+}
+
+openDB.onsuccess=function(e){
+	db=e.target.result;
+	console.log("Desplegada");
+}
+
+function addItem(){
+	let transaction=db.transaction("producto","readwrite");
+	let prod= transaction.objectStore("producto");
+	let pr ={
+		product: "Si",
+		marca:"Ford",
+		precio:20.29,
+		estado:"Bien"
+	}
+	let requestAdd=prod.add(pr);
+	console.log(transaction);
+
 }
