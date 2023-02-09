0edb08149812d74af22847dc14a13a20bdc83d0b
Daniel Bittan Graells <danielbittang@gmail.com>
2023-02-09 23:56:41 +0100

    update

--- Almacenamiento/indexDb-CRUD/js/script.js
+++ Almacenamiento/indexDb-CRUD/js/script.js
@@ -1,10 +1,11 @@
 "use strict"
-
-//Un click al boton sube las cookies.
+//CARGA LOS DATOS A LA BASE DE DATOS
 cargar.addEventListener("click",addItem);
 
-//Envía los datos a la tabla creando una nueva fila
-
+//BORRA LOS DATOS AL ENVIAR EL ID DEL BOTON COMO LA CLAVE DEL OBJETO
+while(i>0){
+    i.addEventListener("click", deleteItem);
+}
 /**
  * Obtenemos los valores de las cookies y los convertimos en objetos de nuevo, luego recorremos un forEach que crea una fila por cada objeto creado.
  **/
--- Almacenamiento/indexDb-CRUD/js/storage.js
+++ Almacenamiento/indexDb-CRUD/js/storage.js
@@ -2,6 +2,7 @@
 
 let openDB=indexedDB.open("productos",2);
 let db;
+let i=1;
 
 openDB.onerror=function(){
 	console.log('Error de indexDB: ${requestDB.errorCode}');
@@ -30,15 +31,42 @@ function addItem(){
 		precio: pre.value,
 		estado:state.value
 	}
-	pdto.add(pr);
-	pdto.onsuccess=(e)=>{
+	let succ=pdto.add(pr);
+	succ.onsuccess=(e)=>{
 		console.log("Conseguido");
 	}
 }
 
 function readAll(){
-	const trans=db.transaction("producto","readonly");
-	let pdto= trans.objectStore("producto");
-	let mostrar=pdto.get(1);
-	console.log(mostrar);
+	const trans = db.transaction("producto","readonly");
+	let pdto = trans.objectStore("producto");
+	let request = pdto.openCursor();
+
+	request.onsuccess = (e) => {
+		let cursor = e.target.result;
+		if(cursor){
+			let pro = cursor.value.product;
+			let bra = cursor.value.marca;
+			let pri = cursor.value.precio;
+			let sta = cursor.value.estado;
+			let div = "<tr class='bg-white border-b dark:bg-gray-800 dark:border-gray-700'><th scope='row' class='px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white'>"+pro+"</th><td class='px-6 py-4'>"+bra+"</td><td class='px-6 py-4'>"+pri+"</td><td class='px-6 py-4'>"+sta+"</td><td><button id='"+i+"' class='focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900'>Borrar</td></tr>";
+			i++;
+			cuerpo.innerHTML += div;
+			cursor.continue();
+		}
+	}
+}
+
+function deleteItem(e){
+	let dIndex = e.target.id;
+	const trans = db.transaction("producto","readwrite");
+	let pdto = trans.objectStore("producto");
+	let succ = pdto.delete(dIndex);
+	succ.onsuccess = () =>{
+		console.log("Elemento borrado");
+	}
+}
+
+function updateData(){
+	
 }
